0ee57e17862b5cc7ed0e7c262c295f8f32481e0b
sebasalbert <sebasalbertperez@gmail.com>
2026-01-08 10:50:05 +0100

    proyectoWEB

---
+++ CLIENTES/USUARIOS.php
@@ -0,0 +1,25 @@
+<?php
+
+class usuarios {
+
+    private $email;
+    private $nombre;
+    private $apellido;
+    private $telefono;
+
+
+    // Getter mágico para leer cualquier propiedad privada
+    public function __get($atributo) {
+        if (property_exists($this, $atributo)) {
+            return $this->$atributo;
+        }
+    }
+
+    // Setter mágico para modificar cualquier propiedad privada
+    public function __set($atributo, $valor) {
+        if (property_exists($this, $atributo)) {
+            $this->$atributo = $valor;
+        }
+    }
+
+}
