e577c54292f95149b637a43c4c4c1b3d8e77a50d
sebasalbert <sebasalbertperez@gmail.com>
2026-01-10 19:06:12 +0100

    actualizado el codigo

--- CLIENTES/conexion.php
+++ CLIENTES/conexion.php
@@ -8,9 +8,6 @@ try {
     // Crear la instancia PDO
     $pdo = new PDO($dsn, $usuario, $pass);
    
-    // Configurar para que lance excepciones en caso de error
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   
     echo "¡Conexión exitosa!";
    } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
