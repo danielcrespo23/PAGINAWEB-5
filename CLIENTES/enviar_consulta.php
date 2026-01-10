c7d6ab03f0fd0840c224c0ae351b5a5ab21fd819
sebasalbert <sebasalbertperez@gmail.com>
2026-01-10 20:01:40 +0100

    empezado el archivo que recibe los datos del form

--- CLIENTES/enviar_consulta.php
+++ CLIENTES/enviar_consulta.php
@@ -1,2 +1,11 @@
 <?php
 
+require_once 'conexion.php';
+
+if ($_SERVER['REQUEST_METHOD'] == $_POST) {
+    $correo    = $_POST['correo'];
+    $nombre    = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $telefono  = $_POST['telefono'];
+    $grado     = $_POST['grado'];
+}
