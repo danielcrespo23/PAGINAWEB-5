6475797871aee42bdc10340af97b8c28cde870f1
sebasalbert <sebasalbertperez@gmail.com>
2026-01-12 11:51:31 +0100

    nuevo dato grado

--- CLIENTES/enviar_consulta.php
+++ CLIENTES/enviar_consulta.php
@@ -7,18 +7,20 @@ if ($_SERVER['REQUEST_METHOD'] == $_POST) {
     $nombre    = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $telefono  = $_POST['telefono'];
+    $grados = $_POST['grados'];
 }
 
 try {
-    $sql = "INSERT INTO USUARIOS (email, nombre, apellido, telefono) 
-        VALUES (:email, :nombre, :apellido, :telefono)";
+    $sql = "INSERT INTO USUARIOS (email, nombre, apellido, telefono, grados) 
+        VALUES (:email, :nombre, :apellido, :telefono, :grados)";
     $stmt = $pdo->prepare($sql);
 
     $stmt->execute([
         ':email' => $email,
         ':nombre' => $nombre,
         ':apellido'  => $apellido,
-        ':telefono' => $telefono
+        ':telefono' => $telefono,
+        ':grados' => $grados
     ]);
 
 } catch (PDOException $e) {
