bca356ff75e43c4f0d24e4fab591d1e0894d8db2
sebasalbert <sebasalbertperez@gmail.com>
2026-01-15 13:50:40 +0100

    nombre modified y completando AccesoDatos

--- dat/AccesoDatos.php
+++ dat/AccesoDatos.php
@@ -6,4 +6,33 @@ include_once __DIR__ . '/USUARIOS.php';
 class AccesoDatos {
     private static $modelo = null;
     private $dbh = null;
-}
+
+    public static function getModelo() {
+        if (self::$modelo == null){
+            self::$modelo = new AccesoDatos();
+        }
+        return self::$modelo;
+    }
+
+    public static function closeModelo(){
+        if (self::$modelo != null){
+            $obj = self::$modelo;
+            $obj->dbh = null;
+            self::$modelo = null;
+        }
+    }
+
+    //Configuración de la base de datos
+    public function __construct() {
+        try {
+            $dns = 'mysql:host='.SERVER_DB.';dbname='.DATABASE_NAME;
+            $this->dbh = new PDO($dns, DB_USER, '');
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Error de conexión con la base de datos: ".$e->getMessage();
+            exit();
+        }
+    }
+
+    
+ }
